cdb4c5910a8b7475fb74d03925ac8c4a05379352
Ibra <70988546+ibra-dotcom@users.noreply.github.com>
2024-04-07 23:23:30 -0400

    Update codeSnippet.py

--- codeSnippet.py
+++ codeSnippet.py
@@ -1,10 +1,10 @@
+#Factorial of a number using recursion:def factorial(n):
 #simple function  
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-
 # Example usage:
 num = 8
 print("Factorial of", num, "is", factorial(num))
