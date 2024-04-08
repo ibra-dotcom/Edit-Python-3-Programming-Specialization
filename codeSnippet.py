ea3a11387cc5aaafd79701306401781c0f152fd5
Ibra <70988546+ibra-dotcom@users.noreply.github.com>
2024-04-07 23:36:47 -0400

    Update codeSnippet.py

--- codeSnippet.py
+++ codeSnippet.py
@@ -11,4 +11,7 @@ print("Factorial of", num, "is", factorial(num))
 
 #Note on recursion: 
 
-
+#We define factorial(n) to calculate the factorial of n.
+#If n is 0, the factorial is 1 (base case).
+#Otherwise, we return n times factorial(n-1), which is a smaller version of the same problem.
+#This process continues until n reaches 0, and then it starts combining the results to calculate the factorial.
